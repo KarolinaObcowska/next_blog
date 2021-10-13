550a1a03399a592329699988ed8d5f660e40d0f1
Karolina Obcowska <jimmy.obcowska@gmail.com>
2021-10-13 13:39:44 +0200

    contact form added

--- next_blog/pages/aboutme/index.js
+++ next_blog/pages/aboutme/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const AboutMe = () => {
   return (
-    <div className="mt-10 lg:px-40 px-10 bg-white dark:bg-gray-900 text-black dark:text-white">
+    <div className="mt-10 flex flex-wrap items-center justify-center lg:px-40 px-10 bg-white dark:bg-gray-900 text-black dark:text-white">
       <h1>About me</h1>
     </div>
   )
